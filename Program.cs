377e868ac0c9035eb16dbd819891be60cf856444
Lykrat <emil.treptow@outlook.com>
2022-11-06 15:10:55 +0100

    La till ID, skrev ut mer methods och la till namn

--- Program.cs
+++ Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
-using static System.Net.Mime.MediaTypeNames;
 
+//Emil SUT22
 namespace Class_Arv_Labb
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Lejon Lion1 = new Lejon(10,"Gul","M",200,1);
+            Lejon Lion1 = new Lejon(10,"Gul","M",200,1,1);
             Lion1.getInfo();
             Lion1.makeSound();
             Lion1.seeAnimal();
             Lion1.checkWeight();
             Lion1.checkEnclosure();
 
-            Zebra Zebra1 = new Zebra(10, "Svart och vit", "F", 60, 4);
+            Zebra Zebra1 = new Zebra(10, "Svart och vit", "F", 60, 4,4);
             Zebra1.makeSound();
+            Zebra1.checkStripes();
 
-            Fisk Fish1 = new Fisk(3, "Guldig", "M", 33, 7);
+            Fisk Fish1 = new Fisk(3, "Guldig", "M", 33, 7,3);
             Fish1.makeSound();
+            Fish1.checkLength();
 
-            Haj Shark1 = new Haj(33, "Mörk blå", "F", 460, 7, 220);
+            Haj Shark1 = new Haj(33, "Mörk blå", "F", 460, 7, 220,1);
             Shark1.makeSound();
+            Shark1.checkTeeth();
+            Shark1.checkTeeth();
 
-            Lax Salmon1 = new Lax(11, "Blå, vit och rosa", "F", 100, 6, 0.7m);
+            Lax Salmon1 = new Lax(11, "Blå, vit och rosa", "F", 100, 6, 0.7m,1);
             Salmon1.makeSound();
+            Salmon1.checkSpeed();
 
         }
     }
@@ -32,6 +37,7 @@ namespace Class_Arv_Labb
     public class Djur
     {
         public int age=default;
+        public int id = default;
         public string species=default;
         public string colour=default;
         public string gender = default;
@@ -41,6 +47,7 @@ namespace Class_Arv_Labb
         public Djur()
         {
             age = 0;
+            id = 0;
             species = "None";
             colour = "None";
             gender = "None";
@@ -53,16 +60,16 @@ namespace Class_Arv_Labb
 
         public void getInfo()
         {
-            Console.WriteLine($"Art: {species}\nFärg: {colour}\nKön: {gender}\nÅlder: {age}år\n");
+            Console.WriteLine($"Art: {species} {id}\nFärg: {colour}\nKön: {gender}\nÅlder: {age}år\n");
         }
 
         public void seeAnimal()
         {
-            Console.WriteLine($"Ser ut som ett {species} som har färgen {colour}.\n");
+            Console.WriteLine($"Ser ut som ett {species} {id} som har färgen {colour}.\n");
         }
         public void checkEnclosure()
         {
-            Console.WriteLine($"{species} är i inhägnad {enclosure}.\n");
+            Console.WriteLine($"{species} {id} är i inhägnad {enclosure}.\n");
         }
     }
     //Child class lion
@@ -76,9 +83,10 @@ namespace Class_Arv_Labb
             species = "Lejon";
             sound = "Roar";
         }
-        public Lejon(int _age,string _colour,string _gender,int _weight,int _enclosure)
+        public Lejon(int _age,string _colour,string _gender,int _weight,int _enclosure,int _id)
         {
             age = _age;
+            id= _id;
             colour = _colour;
             gender = _gender;
             weight = _weight;
@@ -88,21 +96,21 @@ namespace Class_Arv_Labb
         }
         public override void makeSound()
         {
-            Console.WriteLine($"{species} gör ljudet {sound}\n");
+            Console.WriteLine($"{species} {id} gör ljudet {sound}\n");
         }
         public void checkWeight()
         {
             if (weight > 200)
             {
-                Console.WriteLine($"{species} är övervigtig på {weight}kg\n");
+                Console.WriteLine($"{species} {id} är övervigtig på {weight}kg\n");
             }
             else if(weight < 100)
             {
-                Console.WriteLine($"{species} är underviktig {weight}kg\n");
+                Console.WriteLine($"{species} {id} är underviktig {weight}kg\n");
             }
             else
             {
-                Console.WriteLine($"{species} väger {weight}kg\n");
+                Console.WriteLine($"{species} {id} väger {weight}kg\n");
             }
         }
 
@@ -118,9 +126,10 @@ namespace Class_Arv_Labb
             species = "Fisk";
             sound = "blub blub";
         }
-        public Fisk(int _age, string _colour, string _gender, int _length, int _enclosure)
+        public Fisk(int _age, string _colour, string _gender, int _length, int _enclosure, int _id)
         {
             age = _age;
+            id = _id;
             colour = _colour;
             gender = _gender;
             length = _length;
@@ -132,16 +141,16 @@ namespace Class_Arv_Labb
         {
             if (length > 50)
             {
-                Console.WriteLine($"Stora {species} är {length}cm lång.");
+                Console.WriteLine($"Stora {species} {id} är {length}cm lång.\n");
             }
             else
             {
-                Console.WriteLine($"{species} är {length}cm lång");
+                Console.WriteLine($"{species} {id} är {length}cm lång\n");
             }
         }
         public override void makeSound()
         {
-            Console.WriteLine($"{species} gör ljudet {sound} i vatten\n");
+            Console.WriteLine($"{species} {id} gör ljudet {sound} i vatten\n");
         }
     }
     //Double child class shark
@@ -155,9 +164,10 @@ namespace Class_Arv_Labb
             sound = "blob";
             teeth = 0;
         }
-        public Haj(int _age, string _colour, string _gender, int _length, int _enclosure,int _teeth)
+        public Haj(int _age, string _colour, string _gender, int _length, int _enclosure,int _teeth, int _id)
         {
             age = _age;
+            id= _id;
             colour = _colour;
             gender = _gender;
             length = _length;
@@ -170,11 +180,11 @@ namespace Class_Arv_Labb
         {
             if (teeth < 220)
             {
-                Console.WriteLine($"{species} har {teeth} tänder.");
+                Console.WriteLine($"{species} {id} har {teeth} tänder.\n");
             }
             else
             {
-                Console.WriteLine($"{species} har {teeth} tänder. Mindre en vanligt");
+                Console.WriteLine($"{species} {id} har {teeth} tänder. Mindre en vanligt\n");
             }
         }
     }
@@ -189,9 +199,10 @@ namespace Class_Arv_Labb
             sound = "blob blub";
             speed = 0;
         }
-        public Lax(int _age, string _colour, string _gender, int _length, int _enclosure, decimal _speed)
+        public Lax(int _age, string _colour, string _gender, int _length, int _enclosure, decimal _speed, int _id)
         {
             age = _age;
+            id = _id;
             colour = _colour;
             gender = _gender;
             length = _length;
@@ -204,11 +215,11 @@ namespace Class_Arv_Labb
         {
             if (speed < 0.5m)
             {
-                Console.WriteLine($"{species} har maxhastigheten {speed}m/s.");
+                Console.WriteLine($"{species} {id} har maxhastigheten {speed}m/s.\n");
             }
             else
             {
-                Console.WriteLine($"{species} har en hög maxhastighet på {speed}m/s");
+                Console.WriteLine($"{species} {id} har en hög maxhastighet på {speed}m/s\n");
             }
         }
     }
@@ -223,9 +234,10 @@ namespace Class_Arv_Labb
             species = "Zebra";
             sound = "Bark";
         }
-        public Zebra(int _age, string _colour, string _gender, int _stripes, int _enclosure)
+        public Zebra(int _age, string _colour, string _gender, int _stripes, int _enclosure,int _id)
         {
             age = _age;
+            id= _id;
             colour = _colour;
             gender = _gender;
             stripes = _stripes;
@@ -235,21 +247,21 @@ namespace Class_Arv_Labb
         }
         public override void makeSound()
         {
-            Console.WriteLine($"{species} gör ljudet {sound}\n");
+            Console.WriteLine($"{species} {id} gör ljudet {sound}\n");
         }
         public void checkStripes()
         {
             if (stripes > 50)
             {
-                Console.WriteLine($"{species} har {stripes} ränder vilket är mer en vanligt.");
+                Console.WriteLine($"{species} {id} har {stripes} ränder vilket är mer en vanligt.\n");
             }
             else if (stripes < 30)
             {
-                Console.WriteLine($"{species} har {stripes} ränder vilket inte är så många.");
+                Console.WriteLine($"{species} {id} har {stripes} ränder vilket inte är så många.\n");
             }
             else
             {
-                Console.WriteLine($"{species} har {stripes} ränder");
+                Console.WriteLine($"{species} {id} har {stripes} ränder\n");
             }
         }
     }
